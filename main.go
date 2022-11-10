95ce651c6cd1f43132f7764eba74da301485ccff
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2022-11-10 20:31:14 +0100

    second commit

--- main.go
+++ main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	. "fmt"
-	"log"
 	"net/http"
-	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	Println(r.Form) //print form information on server side
-	Println("path", r.URL.Path)
-	Println("scheme", r.URL.Scheme)
-	Println(r.Form["url_long"])
-	for l, m := range r.Form {
-		Println("key:", l)
-		Println("val:", strings.Join(m, ""))
-	}
-	Fprintf(w, "Hello Juwon!") //send data to client side
+type MyMux struct{
 }
 
-func main() {
-	http.HandleFunc("/", sayhelloName)       //set router
-	err := http.ListenAndServe(":9090", nil) //set listen port
-	if err != nil {
-		log.Fatal("ListenAndServer: ", err)
+func (p *MyMux) ServerHTTP(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path=="/"{
+		sayhelloName(w, r)
+		return
 	}
+	http.NotFound(w,r)
+	return
+}
+
+
+func sayhelloName(w http.ResponseWriter, r *http.Request){
+	Fprintf(w,"Hello Juwon")
+}
+
+func main(){
+	mux:=&MyMux{}
+	http.ListenAndServe(":9090",mux)
 }
