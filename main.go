e986de41ed1698bf60bcb54bd38ed7b44afd10b3
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2022-11-13 14:57:47 +0100

    third commit

--- main.go
+++ main.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	. "fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"strings"
 )
-
-type MyMux struct{
-}
-
-func (p *MyMux) ServerHTTP(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path=="/"{
-		sayhelloName(w, r)
-		return
-	}
-	http.NotFound(w,r)
-	return
-}
-
-
 func sayhelloName(w http.ResponseWriter, r *http.Request){
-	Fprintf(w,"Hello Juwon")
+	r.ParseForm()//parse url parameters passed, then parse the response packet for post body
+	Println(r.Form)//print info on the server side
+	Println("path",r.URL.Path)
+	Println("scheme", r.URL.Scheme)
+	Println(r.Form["url_long"])
+	for k,v:=range r.Form{
+		Println("key",k)
+		Println("val:",strings.Join(v,""))
+	}
+	Fprint(w, "Hello Juwon!")//data written to response
+}
+
+func login(w http.ResponseWriter, r *http.Request){
+	Println("method:",r.Method)//get the request method
+	if r.Method=="GET"{
+		t,_:=template.ParseFiles("login.gtpl")
+		t.Execute(w,nil)
+	}else{
+		r.ParseForm()
+		//login in the login
+		Println("username",r.Form["username"])
+		Println("password",r.Form["password"])
+	}
 }
 
 func main(){
-	mux:=&MyMux{}
-	http.ListenAndServe(":9090",mux)
+	http.HandleFunc("/",sayhelloName)//set router rules
+	http.HandleFunc("login",login)
+	err:=http.ListenAndServe(":9090",nil)//set listening port
+	if err !=nil{
+		log.Fatal("ListenAndServe:",err)
+	}
 }
