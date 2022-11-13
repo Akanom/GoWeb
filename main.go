110537e6d6118e9ec9c3ead8ab0feb6c3990b7a3
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2022-11-13 22:57:13 +0100

    fifth commit

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -51,9 +52,39 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// uploading files like pictures to the server
+func upload(w http.ResponseWriter, r *http.Request) {
+	Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 12))
+		token := Sprintf("%x", h.Sum(nil))
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 22)
+		file, handle, err := r.FormFile("uploadfile")
+		if err != nil {
+			Println(err)
+			return
+		}
+		defer file.Close()
+		Fprintf(w, "%v", handle.Header)
+		f, err := os.OpenFile("./test/"+handle.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName) //set router rules
 	http.HandleFunc("login", login)
+	http.HandleFunc("upload", upload)
 	err := http.ListenAndServe(":9090", nil) //set listening port
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
